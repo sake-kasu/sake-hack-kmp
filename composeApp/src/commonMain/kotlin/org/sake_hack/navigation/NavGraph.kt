0f3b872c9a83221ac3e85268be3436b960ef818c
takahiro.maki <j39g8bxh@gmail.com>
2026-01-31 21:38:56 +0900

    update:⚡NavGraphにModalNavigationDrawerを統合(#3)

--- composeApp/src/commonMain/kotlin/org/sake_hack/navigation/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/org/sake_hack/navigation/NavGraph.kt
@@ -3,6 +3,10 @@ package org.sake_hack.navigation
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -10,7 +14,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
+import org.koin.compose.viewmodel.koinViewModel
 import org.sake_hack.ui.components.CommonAppBar
+import org.sake_hack.ui.components.NavigationDrawer
+import org.sake_hack.ui.drawer.DrawerIntent
+import org.sake_hack.ui.drawer.DrawerViewModel
 import org.sake_hack.ui.sakelist.SakeListScreen
 import org.sake_hack.ui.stocklist.StockListScreen
 
@@ -19,70 +28,109 @@ fun SakeAppNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = NavDestinations.STOCK_LIST,
-        modifier = modifier
+    val drawerViewModel: DrawerViewModel = koinViewModel()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    val drawerUiState by drawerViewModel.uiState.collectAsState()
+
+    // ドロワー開閉のIntent連携
+    LaunchedEffect(drawerState.currentValue) {
+        if (drawerState.currentValue == DrawerValue.Open) {
+            drawerViewModel.handleIntent(DrawerIntent.OpenDrawer)
+        } else {
+            drawerViewModel.handleIntent(DrawerIntent.CloseDrawer)
+        }
+    }
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            NavigationDrawer(
+                uiState = drawerUiState,
+                onIntent = drawerViewModel::handleIntent,
+                onNavigate = { destination ->
+                    navController.navigate(destination) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                    scope.launch { drawerState.close() }
+                }
+            )
+        }
     ) {
-        composable(NavDestinations.SAKE_LIST) {
-            SakeListScreen(
-                onNavigate = { route ->
-                    navController.navigate(route) {
-                        // Pop up to start destination to avoid building up a large back stack
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = true
+        NavHost(
+            navController = navController,
+            startDestination = NavDestinations.STOCK_LIST,
+            modifier = modifier
+        ) {
+            composable(NavDestinations.SAKE_LIST) {
+                SakeListScreen(
+                    onNavigate = { route ->
+                        navController.navigate(route) {
+                            // Pop up to start destination to avoid building up a large back stack
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            // Avoid multiple copies of the same destination
+                            launchSingleTop = true
+                            // Restore state when reselecting a previously selected item
+                            restoreState = true
                         }
-                        // Avoid multiple copies of the same destination
-                        launchSingleTop = true
-                        // Restore state when reselecting a previously selected item
-                        restoreState = true
-                    }
-                }
-            )
-        }
+                    },
+                    onMenuClick = { scope.launch { drawerState.open() } }
+                )
+            }
 
-        composable(NavDestinations.STOCK_LIST) {
-            StockListScreen(
-                onNavigate = { route ->
-                    navController.navigate(route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = true
+            composable(NavDestinations.STOCK_LIST) {
+                StockListScreen(
+                    onNavigate = { route ->
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
-        }
+                    },
+                    onMenuClick = { scope.launch { drawerState.open() } }
+                )
+            }
 
-        composable(NavDestinations.HOME) {
-            PlaceholderScreen(
-                title = "ホーム",
-                onNavigate = { route ->
-                    navController.navigate(route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = true
+            composable(NavDestinations.HOME) {
+                PlaceholderScreen(
+                    title = "ホーム",
+                    onNavigate = { route ->
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
-        }
+                    },
+                    onMenuClick = { scope.launch { drawerState.open() } }
+                )
+            }
 
-        composable(NavDestinations.SETTINGS) {
-            PlaceholderScreen(
-                title = "設定",
-                onNavigate = { route ->
-                    navController.navigate(route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = true
+            composable(NavDestinations.SETTINGS) {
+                PlaceholderScreen(
+                    title = "設定",
+                    onNavigate = { route ->
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
+                    },
+                    onMenuClick = { scope.launch { drawerState.open() } }
+                )
+            }
         }
     }
 }
@@ -91,13 +139,14 @@ fun SakeAppNavGraph(
 private fun PlaceholderScreen(
     title: String,
     onNavigate: (String) -> Unit,
+    onMenuClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Scaffold(
         topBar = {
             CommonAppBar(
                 title = title,
-                onMenuClick = { /* TODO: ドロワーメニュー実装 */ }
+                onMenuClick = onMenuClick
             )
         },
         modifier = modifier
